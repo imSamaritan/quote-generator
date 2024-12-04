59eb1754ef66e967f585a4583ab10f5f4b3a8bf6
imsamaritan <aicodemate.talk@gmail.com>
2024-12-04 22:14:52 +0200

    completed the functionality, fetching new quote, render it and share it on twitter

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+//Querying html elements
+const quoteCardEl = document.getElementById('quote-card');
+const quoteTextEl = document.getElementById('quote-text');
+const quoteAuthorEl = document.querySelector('#quote-author span');
+
+const newQuoteBtnEl = document.getElementById('btn-new-quote');
+const tweetBtnEl = document.getElementById('btn-tweet-quote'); 
+
+//Declaring and initializing quote variable to empty list
 let quotes = [];
 
-function filterQuote() {
+function render(quote) {
+  if (!quote.author) {
+    quoteAuthorEl.textContent = 'Unknown';
+  }
+  else {
+    quoteAuthorEl.textContent = quote.author;
+  }
+
+
+  if (quote.text.length > 120) {
+    quoteTextEl.className = 'quote-long';
+  }
+  else {
+    quoteTextEl.className = null;
+  }
+
+  quoteTextEl.textContent = quote.text;
+
+}
+
+function getQuote() {
   const id = Math.floor(Math.random() * quotes.length) + 1;
-  console.log(quotes[id]);
+  render(quotes[id]);
+}
+
+function tweetQuote() {
+  const apiTwitterUrl = `https://twitter.com/intent/tweet/?text=${quoteTextEl.textContent} - ${quoteAuthorEl.textContent}`;
+  window.open(apiTwitterUrl, '_blank');
 }
 
 async function getQuotes() {
@@ -11,12 +45,17 @@ async function getQuotes() {
     const req = await fetch(apiUrl);
     quotes = await req.json();
 
-    filterQuote();
+    getQuote();
   }
   catch (error) {
     throw new Error(error);
   }
 }
 
+//Add event listeners on the buttons
+newQuoteBtnEl.addEventListener('click', getQuote);
+tweetBtnEl.addEventListener('click', tweetQuote);
+
+//Fetching data
 getQuotes();
 
