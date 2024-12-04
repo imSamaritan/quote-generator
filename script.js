8c1629193427682bd1b4717d5b76e46b7f31eb4e
imsamaritan <aicodemate.talk@gmail.com>
2024-12-04 20:02:26 +0200

    initial commit with data fetch

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const btnNewQuote = document.querySelector('#btn-new-quote');
-const btnTweatQuote = document.querySelector('#btn-new-quote');
-
 let quotes = [];
 
-function getQuote() {
-  const randomId = Math.floor(Math.random() * quotes.length);
-  const quote = quotes[randomId];
-  
-  console.log(quote)
+function filterQuote() {
+  const id = Math.floor(Math.random() * quotes.length) + 1;
+  console.log(quotes[id]);
 }
 
-async function getQuotes () {
-  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const req = await fetch(apiUrl);
     quotes = await req.json();
 
-    // get only one quote using a random [id] number
-    getQuote()
-    console.log(quotes)
-  } catch (error) {
+    filterQuote();
+  }
+  catch (error) {
     throw new Error(error);
   }
-
 }
+
+getQuotes();
